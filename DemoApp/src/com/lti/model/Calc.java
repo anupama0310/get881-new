42a10cc5ab72a0dffc4e9cc9777822b69ed7d27a
ANUPAMA <ANUPAMA@DESKTOP-K2N8JJ9>
2020-12-18 16:06:17 +0530

    first commit

---
+++ DemoApp/src/com/lti/model/Calc.java
@@ -0,0 +1,12 @@
+package com.lti.model;
+
+public class Calc {
+
+	public void add(int num1, int num2){
+		System.out.println("Sum="+(num1+num2));
+	}
+	public void sub(int num1, int num2){
+		System.out.println("Differnce="+(num1-num2));
+	}
+
+}
---
+++ DemoApp/src/com/lti/model/Sample.java
@@ -0,0 +1,11 @@
+package com.lti.model;
+
+public class Sample {
+	public void show(){
+		System.out.println("Hello World");
+	}
+
+	public void method(){
+		System.out.println("Hi Alllllllllllllllll");
+	}
+}
