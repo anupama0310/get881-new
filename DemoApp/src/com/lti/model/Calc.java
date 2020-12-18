5ce53bde0a7268846fa9e1f33f862b6a914d3072
ANUPAMA <ANUPAMA@DESKTOP-K2N8JJ9>
2020-12-18 16:41:48 +0530

    added

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -8,5 +8,7 @@ public class Calc {
 	public void sub(int num1, int num2){
 		System.out.println("Differnce="+(num1-num2));
 	}
-
+	public void mul(int num1, int num2){
+		System.out.println("Multiplication="+(num1*num2));
+	}
 }
